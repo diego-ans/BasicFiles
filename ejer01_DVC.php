081ea6670da73ca47c10d8a83a8a30256141523a
Diegoans337 <83733071+Diegoans337@users.noreply.github.com>
2022-02-23 11:17:23 -0600

    Add files via upload

---
+++ ejer01_DVC.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Autor: Diego varela colin
+Grupo: 4PAM
+Fecha: 17/02/2022
+Contacto: diegovarela,2005@gmail.com
+Descripcion: El programa trata de hacer uso de las instrucciones
+print, printf y echo para hacer una presentacion de mi mismo
+*/
+
+echo "Mi nombre es Diego varela colin \t";
+
+printf("Tengo %d años \t",16);
+print("Estudio programacion en una preparatoria tecnica\t");
+?>
---
+++ ejer04_DVC.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Autor: Diego varela colin
+Grupo: 4PAM
+Fecha: 17/02/2022
+Contacto: diegovarela,2005@gmail.com
+Descripcion: El programa trata de hacer la serie fibonacci con las tres estructuras
+repetitivas
+*/
+
+$total=10;
+
+$NUM1;
+$NUM2;
+$Aux;
+for($i=0;$i<=10;$i++){
+    $NUM1=$i-1;
+    $NUM2=$i-2;
+    printf("%d <br>",$NUM1+$NUM2);
+
+}
+
+
+
+?>
